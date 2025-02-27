fd223e836690bb96aa6cba6a9b51ed620be1950f
authorisnowhere <authorisnowhere@gmail.com>
2025-02-27 10:17:01 +0700

    feat: setup broilerplate next js 15

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,34 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+  }),
+  {
+    rules: {
+      'capitalized-comments': ['error', 'always'],
+    },
+  },
+  {
+    ignores: [
+      '.config/*',
+      '**/node_modules/',
+      '**/dist/',
+      '**/build/',
+      '**/out/',
+      '**/lib/',
+      '**/public/',
+    ],
+  },
+]
 
-export default eslintConfig;
+export default eslintConfig
