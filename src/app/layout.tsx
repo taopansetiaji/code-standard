ac2966b73fb0b0220716dc39117167ddb49abb4d
authorisnowhere <authorisnowhere@gmail.com>
2025-03-01 14:15:32 +0700

    fix promise params on next-intl

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,11 +6,14 @@ import Footer from '@/components/organisms/footer'
 
 export default async function RootLayout({
   children,
+  params,
 }: {
   children: React.ReactNode
+  params: Promise<{ locale: string }>
 }) {
+  const { locale } = await params
   return (
-    <html lang='en' suppressHydrationWarning>
+    <html lang={locale} suppressHydrationWarning>
       <body>
         <ThemeProvider
           attribute='class'
